b52df3cd4f186a13c89265bdfe9e7c4bbbef2297
jiangyixin77 <166776117+jiangyixin77@users.noreply.github.com>
2024-08-08 00:18:34 +0800

    Update rpc_provider.h

--- src/include/rpc_provider.h
+++ src/include/rpc_provider.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "google/protobuf/service.h"
-#include <memory>
+#include <unordered_map>
 #include <muduo/net/TcpServer.h>
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/InetAddress.h>
 #include <muduo/net/TcpConnection.h>
+#include <string>
+#include <functional>
+#include <google/protobuf/descriptor.h>
 
 //框架提供的专门发布RPC服务的网络对象类
 class RpcProvider
@@ -16,7 +19,22 @@ public:
     //启动RPC服务节点，开始提供RPC远程网络调用服务
     void Run();
 private:
-    muduo::net::EventLoop m_eventLoop;//组合了EventLoop
-    void OnConnection(const muduo::net::TcpConnectionPtr&);//新的socket连接回调
-    void OnMessage(const muduo::net::TcpConnectionPtr&,muduo::net::Buffer*,muduo::Timestamp);//已建立连接用户的读写事件回调
+    //组合了EventLoop
+    muduo::net::EventLoop m_eventLoop;
+
+    //service服务类型信息
+    struct ServiceInfo
+    {
+        google::protobuf::Service *m_service; //保存服务对象
+        std::unordered_map<std::string , const google::protobuf::MethodDescriptor*> m_methodMap; //保存服务方法
+    }
+
+    //存储注册成功的服务对象及其方法的所有信息
+    std::unordered_map<std::string , ServiceInfo> m_serviceMap;
+
+    //新的socket连接回调
+    void OnConnection(const muduo::net::TcpConnectionPtr&);
+
+    //已建立连接用户的读写事件回调
+    void OnMessage(const muduo::net::TcpConnectionPtr&,muduo::net::Buffer*,muduo::Timestamp);
 };
