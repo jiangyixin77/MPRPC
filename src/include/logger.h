2a748065aec53e5d67532aa06052d8bfc9719776
jiangyixin77 <166776117+jiangyixin77@users.noreply.github.com>
2024-08-13 00:07:03 +0800

    Update logger.h

--- src/include/logger.h
+++ src/include/logger.h
@@ -24,3 +24,22 @@ private:
   Logger(const Logger&)= delete;
   Logger(Logger&&)=delete;
 };
+// 定义宏
+#define LOG INFO(logmsgformat,...)\
+  do \
+  { \
+    Logger &logger = Logger::GetInstance();\
+    logger.SetLogLevel(INFO);\
+    char c[1024]={0};\
+    snprintf(c,1024,logmsgformat,##__VA_ARGS_);\
+    logger.Log(c);\
+  }while({0});
+#define LOG_ERR(logmsgformat,...)\
+  do \
+  {  \
+    Logger &logger = Logger::GetInstance();\
+    logger.SetLogLevel(ERROR);\
+    char c[1024]={0};\
+    snprintf(c，1024，logmsgformat，##__VA_ARGS__);\
+    logger.Log(c);\
+  }while({0});
